d2be67b3771ff8cc59920faaacce4db2f8bf472f
Burlaca Artiom <114052012+ArtiomBurlaca@users.noreply.github.com>
2022-09-29 11:02:24 +0300

    Update cap1_tema5 prb1.pas

--- cap1_tema5 prb1.pas
+++ cap1_tema5 prb1.pas
@@ -1,10 +1,5 @@
-
-
-
 program Hello;
-
 var a,b,c,d :integer;
-
 begin
   writeln ('dati a ');
   readln (a);
@@ -13,13 +8,8 @@ begin
   writeln ('dati c ');
   readln (c);
   if (a+b>c) and (b+c>a) and (a+c>b)and (a>0)and (b>0)and (c>0) then
-
   d:=a+b+c
   else
   writeln ('nu sunt laturile unui triunghi');
-  writeln ('perimetrul este ', d);
-  
-  
-  
+  writeln ('perimetrul este ', d); 
 end.
-
