a154b7cc62c8aa46be47a2f6d210c5210b4c2dc6
Burlaca Artiom <114052012+ArtiomBurlaca@users.noreply.github.com>
2022-09-29 11:00:56 +0300

    tema din clasa

---
+++ main.pas
@@ -0,0 +1,31 @@
+{
+
+                            Online Pascal Compiler.
+                Code, Compile, Run and Debug Pascal program online.
+Write your code in this editor and press "Run" button to execute it.
+
+}
+
+
+program Hello;
+
+var a,b,c,d :integer;
+
+begin
+  writeln ('dati a ');
+  readln (a);
+  writeln ('dati b ');
+  readln (b);
+  writeln ('dati c ');
+  readln (c);
+  if (a+b>c) and (b+c>a) and (a+c>b)and (a>0)and (b>0)and (c>0) then
+
+  d:=a+b+c
+  else
+  writeln ('nu sunt laturile unui triunghi');
+  writeln ('perimetrul este ', d);
+  
+  
+  
+end.
+
